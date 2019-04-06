565a2cc910902078f3ed2f73dfb32c1e78cfc96a
zynkn <ywnwalone@naver.com>
2019-04-06 18:46:52 +1100

    history go back animation

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,54 @@
 import React, { Component } from 'react';
+import { RouteComponentProps } from 'react-router-dom';
 import Modal from './components/Modal';
 
-class App extends Component {
+class App extends Component<RouteComponentProps, any> {
+
+  constructor(props: RouteComponentProps) {
+    super(props);
+    this.state = {
+      isModalOpen: false,
+    }
+  }
+  componentDidUpdate(prevProps: RouteComponentProps) {
+    if (this.state.isModalOpen && this.props.history.action === "POP") {
+      this.setState({
+        isModalOpen: false,
+      })
+    }
+  }
+  openModal = () => {
+    const { history } = this.props;
+    history.push('/', 'Modal');
+    this.setState({
+      isModalOpen: true
+    })
+  }
+  closeModal = () => {
+    const { history } = this.props;
+    history.goBack();
+    this.setState({
+      isModalOpen: false,
+    })
+  }
   render() {
     return (
       <main className="App">
-        <Modal />
+        <button className="btn" onClick={this.openModal}>Modal Open</button>
+        <Modal isOpen={this.state.isModalOpen} name="Modal-anim">
+
+          <p className="title">Title</p>
+          <div className="content">
+            <p>
+              Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel tempora nulla, non molestiae dicta ducimus. Et unde laborum eveniet ex quod doloribus quae, aliquam beatae atque, vero assumenda rem quo?
+            </p>
+          </div>
+          <div className="button-wrap">
+            <button onClick={this.closeModal}>Confirm</button>
+          </div>
+
+        </Modal>
+
       </main>
     );
   }
--- src/components/Modal/Modal.tsx
+++ src/components/Modal/Modal.tsx
@@ -1,28 +1,55 @@
 import React from 'react';
 import './Modal.scss';
+import ReactCSSTransitionGroup from 'react-addons-css-transition-group'
 
-
-class Modal extends React.Component {
+class Modal extends React.Component<any> {
 
   render() {
     return (
       <>
-        <div className="Modal-overlay"></div>
+        {
+          this.props.isOpen ?
+
+            <ReactCSSTransitionGroup transitionName={this.props.name} transitionEnterTimeout={500}
+              transitionLeaveTimeout={300}>
+              <div className="Modal-overlay" />
+              <div className="Modal">
+                {this.props.children}
+              </div>
+            </ReactCSSTransitionGroup>
+            :
+            <ReactCSSTransitionGroup transitionName={this.props.name} transitionEnterTimeout={500}
+              transitionLeaveTimeout={300} />
+        }
+      </>
+    )
+  }
+}
+
+export default Modal;
+
+
+/*
+      <>
+        <ReactCSSTransitionGroup
+          transitionName="example"
+          transitionEnterTimeout={500}
+          transitionLeaveTimeout={300}>
+
+          <div className="Modal-overlay" onClick={this.props.close}></div>
+        </ReactCSSTransitionGroup>
+
         <div className="Modal">
-          <p className="title">모달입니다.</p>
+          <p className="title">Title</p>
           <div className="content">
             <p>
               Lorem, ipsum dolor sit amet consectetur adipisicing elit. Vel tempora nulla, non molestiae dicta ducimus. Et unde laborum eveniet ex quod doloribus quae, aliquam beatae atque, vero assumenda rem quo?
             </p>
           </div>
           <div className="button-wrap">
-            <button>Confirm</button>
+            <button onClick={this.props.close}>Confirm</button>
           </div>
         </div>
       </>
 
-    )
-  }
-}
-
-export default Modal;
+*/
